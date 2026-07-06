e99935da106c0f9370ae0a8e0ddbf67aef409412
Mohamed Asif <mohamedasif092006@gmail.com>
2026-07-06 20:42:57 +0530

    Fix X pattern screenshot

--- Right_Pascals_Triangle/Right_Pascals_Triangle.java
+++ Right_Pascals_Triangle/Right_Pascals_Triangle.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Right_Pascals_Triangle {
+public class right_pascals_triangle {
     public static void main(String []args){
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter number:");
